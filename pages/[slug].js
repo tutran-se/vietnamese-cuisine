12deaa8e5f33fdb7ba40b33a4682e8020f49d3a2
iamtu <trandinhtu.dev@gmail.com>
2021-05-14 00:57:57 -0400

    Incremental Regeneration

--- pages/[slug].js
+++ pages/[slug].js
@@ -19,6 +19,7 @@ export async function getStaticProps(context) {
 
   return {
     props: { cuisine: items[0].fields }, // will be passed to the page component as props
+    revalidate: 1,
   };
 }
 export async function getStaticPaths() {
--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export async function getStaticProps(context) {
   });
   return {
     props: { cuisines }, // will be passed to the page component as props
+    revalidate: 1,
   };
 }
 
